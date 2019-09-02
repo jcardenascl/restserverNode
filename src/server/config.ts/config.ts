107551f14bd1a72c7a36a50448ed9b33786bfe7e
jcardenascl <jl.danelcardenas@gmail.com>
2019-09-02 00:11:07 -0400

    Variable de entorno MONGO

--- src/server/config.ts/config.ts
+++ src/server/config.ts/config.ts
@@ -6,7 +6,7 @@ const inDebug = process.env.NODE_ENV || 'dev';
 
 export const environment = {
     puerto: process.env.PORT || '3000',
-    urlDB: (inDebug === 'dev') ? 'mongodb://localhost:27017/cafe' : 'process.env.MONGO_URI',
+    urlDB: (inDebug === 'dev') ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI,
     optionsCors: {
         allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
         credentials: true,
